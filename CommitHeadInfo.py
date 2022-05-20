fa03506b20cd9c714fcf686801612656a855b58d
Vishal <bvishal.athreya@gmail.com>
2022-05-20 09:28:49 -0700

    Cleanup DiffTools Add pretty print while writing json to commit_head_pointer.json

--- CommitHeadInfo.py
+++ CommitHeadInfo.py
@@ -7,11 +7,18 @@ class CommitHeadInfo:
     def __init__(self):
         self.head_pointer_file = os.path.join(constants.COMMITS_FOLDER, constants.COMMIT_HEAD_POINTER_FILE)
         if(os.path.exists(self.head_pointer_file)):
-            head_pointer = json.loads(self.head_pointer_file)
+            with open(self.head_pointer_file, "r") as f:
+                self.head_pointer = json.load(f)
         else:
             raise FileNotFoundError(self.head_pointer_file)
 
-        self.global_head = head_pointer["global_head_file"]
-        self.global_tail = head_pointer["global_tail_file"]
-        self.current_head = head_pointer["current_head_file"]
+        self.global_head = self.head_pointer["global_head"]
+        self.global_tail = self.head_pointer["global_tail"]
+        self.current_head = self.head_pointer["current_head"]
+        self.global_head_model_path = self.head_pointer["global_head_model"]
+        self.global_tail_model_path = self.head_pointer["global_tail_model"]
 
+    def update(self, field, value):
+        self.head_pointer[field] = value
+        with open(self.head_pointer_file, "w") as f:
+                f.write(json.dumps(self.head_pointer, indent=4, sort_keys=True))
--- difftools.py
+++ difftools.py
@@ -11,15 +11,13 @@ from typing import *
 from typeguard import check_type
 import constants
 
-import CommitHeadInfo
+from CommitHeadInfo import CommitHeadInfo
 from onnx_functions import AugumentedOnnxModel
 
 Tensor = TypeVar("Tensor", np.ndarray, torch.Tensor)
 
 
 class ModelUpdate:
-    # Fetch Commit Head Info
-    commit_head_info = CommitHeadInfo()
 
     supported_update_types = {
         "dense": Tensor,
@@ -47,96 +45,127 @@ class ModelUpdate:
         self.delta = delta
 
 
-def create_diff_file(updates: Union[List[ModelUpdate], ModelUpdate], commit_head_info) -> str:
+class DiffTools:
+    def __init__(self):
+        # Fetch Commit Head Info
+        self.commit_head_info = CommitHeadInfo()
 
-    if not isinstance(updates, List):
-        updates = [updates]
+    def create_diff_file(self, updates: Union[List[ModelUpdate], ModelUpdate]) -> str:
+        if not isinstance(updates, List):
+            updates = [updates]
 
-    # Fetch Global Head's diff index
-    with open("{}.modeldiff".format(commit_head_info.global_head), "rb") as infile:
-        global_head_diff = pickle.load(infile)
+        # Fetch Global Head's diff index value
+        if(self.commit_head_info.global_head):
+            print("Loading Global Head File -: {}".format(self.commit_head_info.global_head))
+            with open("{}.modeldiff".format(self.commit_head_info.global_head), "rb") as infile:
+                global_head_diff = pickle.load(infile)
 
-    # Each entry in changes should be a 3-tuple (initializer, update_type, delta)
-    update_dict = {
-        "diff_index": global_head_diff['diff_index'] + 1,
-        "previous_diff": commit_head_info.global_head,
-        "next_diff": None,
-        "initializers": {
-            update.initializer_name: (update.update_type, update.delta)
-            for update in updates
+        # Each entry in changes should be a 3-tuple (initializer, update_type, delta)
+        update_dict = {
+            "diff_index": global_head_diff['diff_index'] + 1 if self.commit_head_info.global_head else 1,
+            "previous_diff": self.commit_head_info.global_head,
+            "next_diff": "",
+            "initializers": {
+                update.initializer_name: (update.update_type, update.delta) for update in updates
+            }
         }
-    }
-    hash = hashlib.md5(pickle.dumps(update_dict)).hexdigest()
+        print("New diff chain link (index, prev, next) -:", update_dict["diff_index"], update_dict["previous_diff"], update_dict["next_diff"])
 
-    filename = os.path.join(constants.COMMITS_FOLDER, f"{hash}.modeldiff")
+        hash = hashlib.md5(pickle.dumps(update_dict)).hexdigest()
+        new_diff_name = os.path.join(constants.COMMITS_FOLDER, f"{hash}.modeldiff")
+        print(new_diff_name)
+        # Add name of new diff file to update_dict for easier access
+        update_dict["current_diff"] = new_diff_name
 
-    # Update next diff pointer of current global head & persist
-    global_head_diff['next_diff'] = filename
-    with open(commit_head_info.global_head, "wb") as content_file:
-            pickle.dump(global_head_diff, content_file)
-
-    # Persist new diff
-    with open(filename, "wb") as content_file:
-            pickle.dump(update_dict, content_file)
-
-    return filename
-
-
-def apply_diff_file(augumented_onnx_model, diffname):
-    new_model = copy.deepcopy(augumented_onnx_model)
-
-    with open(diffname, "rb") as infile:
-        update_dict = pickle.load(infile)
-
-    seen_initializers = set()
-    for initializer_name, (update_type, delta) in update_dict["initializers"].items():
-        if initializer_name not in seen_initializers:
-            seen_initializers.add(initializer_name)
+        # Update next diff pointer of current global head & persist
+        if(self.commit_head_info.global_head):
+            print("Updating pointers in chain")
+            global_head_diff['next_diff'] = new_diff_name
+            with open(self.commit_head_info.global_head, "wb") as content_file:
+                    pickle.dump(global_head_diff, content_file)
+            self.commit_head_info.update("global_head", new_diff_name)
         else:
-            raise ValueError(
-                f"An initializer can appear at most once in a modeldiff file: {initializer_name} appears multiple times."
-            )
+            print("New Chain, setting pointers")
+            self.commit_head_info.update("global_head", new_diff_name)
+            self.commit_head_info.update("global_tail", new_diff_name)
 
-        new_model.update_initializer(initializer_name, update_type, delta)
+        # Persist new diff
+        print("Persisting new diff")
+        with open(new_diff_name, "wb") as content_file:
+                pickle.dump(update_dict, content_file)
 
-    return new_model
+        # Apply new diff to model and persist it at head
+        print("Persisting new model")
+        model_head = self.apply_diff_file(AugumentedOnnxModel(self.commit_head_info.global_head_model_path), self.commit_head_info.global_head)
+        model_head.save_model(self.commit_head_info.global_head_model_path)
 
-
-def checkout(diff_file, commit_head_info):
-    """
-    Sequentially applies diffs in order till checkpoint is reached. Order of diffs determined by diff_index of diff_file
-    :param diff_file:
-    :return: ONNX model state at checkpoint defined by diff_file
-    """
-    with open(diff_file, "rb") as infile:
-        checkpoint_diff = pickle.load(infile)
-    checkpoint_diff_index = checkpoint_diff['diff_index']
-
-    with open("{}.modeldiff".format(commit_head_info.global_head), "rb") as infile:
-        global_head_diff = pickle.load(infile)
-
-    with open("{}.modeldiff".format(commit_head_info.global_tail), "rb") as infile:
-        global_tail_diff = pickle.load(infile)
-
-    if((global_head_diff['diff_index'] - checkpoint_diff_index) <= (checkpoint_diff_index - global_tail_diff['diff_index'])):
-        pointer_direction = "prev_diff"
-        curr_model = AugumentedOnnxModel(global_head)
-        current_diff = global_head_diff
-    else:
-        pointer_direction = "next_diff"
-        curr_model = AugumentedOnnxModel(global_tail)
-        current_diff = global_tail_diff
-
-    # Iterate and update using apply_diff_file
-    while(current_diff[pointer_direction] != diff_file):
-        curr_model = apply_diff_file(curr_model, current_diff[pointer_direction])
-        with open("{}.modeldiff".format(current_diff[pointer_direction]), "rb") as infile:
-            current_diff = pickle.load(infile)
+        return new_diff_name
 
 
+    def apply_diff_file(self, augumented_onnx_model: AugumentedOnnxModel, diffname, inplace=False):
+        if(inplace):
+            new_model = augumented_onnx_model
+        else:
+            new_model = copy.deepcopy(augumented_onnx_model)
+
+        with open(diffname, "rb") as infile:
+            update_dict = pickle.load(infile)
+
+        seen_initializers = set()
+        for initializer_name, (update_type, delta) in update_dict["initializers"].items():
+            if initializer_name not in seen_initializers:
+                seen_initializers.add(initializer_name)
+            else:
+                raise ValueError(
+                    f"An initializer can appear at most once in a modeldiff file: {initializer_name} appears multiple times."
+                )
+
+            new_model.update_initializer(initializer_name, update_type, delta)
+
+        return new_model
 
 
-def print_human_readable_diff(diff_hash):
-    with open(f"{diff_hash}.modeldiff.content", "rb") as infile:
-        update_dict = pickle.load(infile)
-    rprint(update_dict)
+    def checkout(self, checkpoint_file):
+        """
+        Sequentially applies diffs in order till checkpoint is reached. Order of diffs determined by diff_index of diff_file
+        :param diff_file:
+        :return: ONNX model state at checkpoint defined by diff_file
+        """
+        checkpoint_file = os.path.join(constants.COMMITS_FOLDER, checkpoint_file)
+        with open(checkpoint_file, "rb") as infile:
+            checkpoint_diff = pickle.load(infile)
+        checkpoint_diff_index = checkpoint_diff['diff_index']
+
+        with open(self.commit_head_info.global_head, "rb") as infile:
+            global_head_diff = pickle.load(infile)
+
+        with open(self.commit_head_info.global_tail, "rb") as infile:
+            global_tail_diff = pickle.load(infile)
+
+        if((global_head_diff['diff_index'] - checkpoint_diff_index) <= (checkpoint_diff_index - global_tail_diff['diff_index'])):
+            pointer_direction = "prev_diff"
+            model = AugumentedOnnxModel(self.commit_head_info.global_head_model_path)
+            # Chain starts from diff before global head diff
+            with open(global_head_diff[pointer_direction], "rb") as infile:
+                diff = pickle.load(infile)
+        else:
+            pointer_direction = "next_diff"
+            model = AugumentedOnnxModel(self.commit_head_info.global_tail_model_path)
+            diff = global_tail_diff
+
+        # Iterate and update until prior to the checkpoint file using apply_diff_file
+        while(diff["current_diff"] != checkpoint_file):
+            model = self.apply_diff_file(model, diff["current_diff"])
+            with open(diff[pointer_direction], "rb") as infile:
+                diff = pickle.load(infile)
+
+        # Apply checkpoint diff file
+        model = self.apply_diff_file(model, diff["current_diff"])
+
+        return model
+
+
+    def print_human_readable_diff(self, diff_hash):
+        with open(f"{diff_hash}.modeldiff.content", "rb") as infile:
+            update_dict = pickle.load(infile)
+        rprint(update_dict)
