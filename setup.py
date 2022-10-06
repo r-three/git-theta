e9e0494b25b540c66f885121d6ae97f0d230f4ce
Colin Raffel <craffel@gmail.com>
2022-10-06 10:50:28 -0400

    Add setup.py (#26)
    * Add initial setup.py
    
    * Install scripts

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="git_cml",
+    version="0.0.0",
+    description="Version control system for model checkpoints.",
+    author="Colin Raffel",
+    author_email="craffel@gmail.com",
+    url="https://github.com/r-three/checkpoint-vcs",
+    packages=[],
+    scripts=["bin/git-ml", "bin/git-ml-filter"],
+    long_description="Version control system for model checkpoints.",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Version Control",
+    ],
+    keywords="git vcs machine-learning",
+    license="MIT",
+    install_requires=[
+        "GitPython",
+    ],
+)
