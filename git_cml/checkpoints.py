77a5f5be5fe7eef90614a5be83db8383bb294a46
Nikhil Kandpal <nkandpa2@gmail.com>
2022-10-12 02:06:37 -0400

    Get git diff to work. Made the following changes: - Change git-cml-filter clean to store diff-able text in the staged   model checkpoint files. That way git diff on the staged files is   meaningful.
    - Implement JSONCheckpoint subclass of Checkpoint for prototyping
      purposes
    
    - change Checkpoint class so that it is initialized
      from dictionary with constructor and from file with Checkpoint.from_file()
    
    - Incorporate Checkpoint subclasses into git-cml and git-cml-filter so
      that we can easily move to using Pytorch/TF Checkpoints in the future
    
    - migrate code for iterating leaves of checkpoint dictionaries and
      parameter groups saved on filesystem into git_cml/checkpoints.py

--- git_cml/checkpoints.py
+++ git_cml/checkpoints.py
@@ -1,27 +1,32 @@
 """Backends for different checkpoint formats."""
 
 import torch
+import os
+import json
+import io
 
 
 class Checkpoint(dict):
     """Abstract base class for wrapping checkpoint formats."""
-
-    def __init__(self, checkpoint_path):
+    
+    @classmethod
+    def from_file(cls, checkpoint_path):
         """Create a new Checkpoint object.
 
         Parameters
         ----------
-        checkpoint_path : str
+        checkpoint_path : str or file-like object
             Path to a checkpoint file
         """
-        super(Checkpoint, self).__init__(self._load(checkpoint_path))
-
-    def _load(self, checkpoint_path):
+        return cls(cls._load(checkpoint_path))
+    
+    @classmethod
+    def _load(cls, checkpoint_path):
         """Load a checkpoint into a dict format.
 
         Parameters
         ----------
-        checkpoint_path : str
+        checkpoint_path : str or file-like object
             Path to a checkpoint file
 
         Returns
@@ -36,21 +41,22 @@ class Checkpoint(dict):
 
         Parameters
         ----------
-        checkpoint_path : str
+        checkpoint_path : str or file-like object
             Path to write out the checkpoint file to
         """
         raise NotImplementedError
-
+    
 
 class PyTorchCheckpoint(Checkpoint):
     """Class for wrapping PyTorch checkpoints."""
-
-    def _load(self, checkpoint_path):
+    
+    @classmethod
+    def _load(cls, checkpoint_path):
         """Load a checkpoint into a dict format.
 
         Parameters
         ----------
-        checkpoint_path : str
+        checkpoint_path : str or file-like object
             Path to a checkpoint file
 
         Returns
@@ -72,8 +78,115 @@ class PyTorchCheckpoint(Checkpoint):
 
         Parameters
         ----------
-        checkpoint_path : str
+        checkpoint_path : str or file-like object
             Path to write out the checkpoint file to
         """
         checkpoint_dict = {k: torch.as_tensor(v) for k, v in self.__dict__.items()}
         torch.save(checkpoint_dict, checkpoint_path)
+
+
+class JSONCheckpoint(Checkpoint):
+    """Class for prototyping with JSON checkpoints"""
+    
+    @classmethod
+    def _load(cls, checkpoint_path):
+        """Load a checkpoint into a dict format.
+
+        Parameters
+        ----------
+        checkpoint_path : str or file-like object
+            Path to a checkpoint file
+
+        Returns
+        -------
+        model_dict : dict
+            Dictionary mapping parameter names to parameter values
+        """
+        if isinstance(checkpoint_path, io.IOBase):
+            return json.load(checkpoint_path)
+        else:
+            with open(checkpoint_path, 'r') as f:
+                return json.load(f)
+
+    def save(self, checkpoint_path):
+        """Load a checkpoint into a dict format.
+
+        Parameters
+        ----------
+        checkpoint_path : str or file-like object
+            Path to write out the checkpoint file to
+        """
+        if isinstance(checkpoint_path, io.IOBase):
+            json.dump(self, checkpoint_path)
+        else:
+            with open(checkpoint_path, 'w') as f:
+                json.dump(self, f)
+
+
+def iterate_dict_leaves(d):
+    """
+    Generator that iterates through a dictionary and produces (leaf, keys) tuples where leaf is a dictionary leaf
+    and keys is the sequence of keys used to access leaf. Dictionary is iterated in depth-first
+    order with lexicographic ordering of keys.
+
+    Example
+    -------
+    d = {'a': {'b': {'c': 10, 'd': 20, 'e': 30}}}
+    iterate_dict_leaves(d) --> ((10, ['a','b','c']), (20, ['a','b','d']), (30, ['a','b','e']))
+
+    Parameters
+    ----------
+    d : dict
+        dictionary to iterate over
+
+    Returns
+    -------
+    generator
+        generates dict leaf, key path tuples
+    """
+    def _iterate_dict_leaves(d, prefix):
+        for k in sorted(d.keys()):
+            v = d[k]
+            if isinstance(v, dict):
+                yield from _iterate_dict_leaves(v, prefix + [k])
+            else:
+                yield (v, prefix + [k])
+
+    return _iterate_dict_leaves(d, [])
+
+
+def iterate_dir_leaves(root):
+    """
+    Generator that iterates through files in a directory tree and produces (path, dirs) tuples where
+    path is the file's path and dirs is the sequence of path components from root to the file.
+
+    Example
+    -------
+    root
+    ├── a
+    │   ├── c
+    │   └── d
+    └── b
+        └── e
+
+    iterate_dir_leaves(root) --> ((root/a/c, ['a','c']), (root/a/d, ['a','d']), (root/b/e, ['b','e']))
+
+    Parameters
+    ----------
+    root : str
+        Root of directory tree to iterate over
+
+    Returns
+    -------
+    generator
+        generates directory tree leaf, subdirectory list tuples
+    """
+    def _iterate_dir_leaves(root, prefix):
+        for d in os.listdir(root):
+            dir_member = os.path.join(root, d)
+            if os.path.isdir(dir_member):
+                yield from _iterate_dir_leaves(dir_member, prefix=prefix + [d])
+            else:
+                yield (dir_member, prefix + [d])
+
+    return _iterate_dir_leaves(root, [])
