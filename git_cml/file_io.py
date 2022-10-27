27b31f063caac11568906042b0947ca31f588b37
nkandpa2 <nkandpa2@gmail.com>
2022-10-27 15:51:44 -0400

    Store parameters as zarr using tensorstore (#45)
    1) Change the API from `git cml init`. Instead of this, we model the API
       off of git-lfs. The user runs `git cml install` which initializes the
       filter driver. When the user has a repo and model they want to track
       with git-cml, they run `git cml track <file>` which writes the
       tracked file to the attributes file.
    2) The filter driver should be initialized globally in ~/.gitconfig and
       tracked checkpoint files should be listed in .gitattributes which
       must be committed to the repo. This makes it so that when a snapshot is
       cloned, the gitconfig is already set up and the gitattributes get
       cloned into the working tree along with the rest of the repo. This
       makes it so that the correct files get smudged upon clone.

---
+++ git_cml/file_io.py
@@ -0,0 +1,103 @@
+import tensorstore as ts
+import io
+import json
+import logging
+
+
+def load_tracked_file(f):
+    """
+    Load tracked file
+
+    Parameters
+    ----------
+    f : str
+        path to file tracked by git-cml filter
+
+    Returns
+    -------
+    np.ndarray
+        numpy array stored in tracked file
+
+    """
+    logging.debug(f"Loading tracked file {f}")
+    ts_file = ts.open(
+        {
+            "driver": "zarr",
+            "open": True,
+            "kvstore": {
+                "driver": "file",
+                "path": f,
+            },
+        }
+    ).result()
+    return ts_file.read().result()
+
+
+def write_tracked_file(f, param):
+    """
+    Dump param into tracked file
+
+    Parameters
+    ----------
+    f : str
+        path to output file
+    param : np.ndarray
+        param value to dump to file
+
+    """
+    logging.debug(f"Dumping param to {f}")
+    ts_file = ts.open(
+        {
+            "driver": "zarr",
+            "kvstore": {
+                "driver": "file",
+                "path": f,
+            },
+            "metadata": {
+                "shape": param.shape,
+                "dtype": param.dtype.str,
+            },
+            "create": True,
+            "delete_existing": True,
+        }
+    ).result()
+    return ts_file.write(param).result()
+
+
+def load_staged_file(f):
+    """
+    Load staged file
+
+    Parameters
+    ----------
+    f : str or file-like object
+        staged file to load
+
+    Returns
+    -------
+    dict
+        staged file contents
+    """
+    if isinstance(f, io.IOBase):
+        return json.load(f)
+    else:
+        with open(f, "r") as f:
+            return json.load(f)
+
+
+def write_staged_file(f, contents):
+    """
+    Write staged file
+
+    Parameters
+    ----------
+    f : str or file-like object
+        file to write staged contents to
+    contents : dict
+        dictionary to write to staged file
+    """
+    if isinstance(f, io.IOBase):
+        json.dump(contents, f, indent=4)
+    else:
+        with open(f, "w") as f:
+            json.dump(contents, f, indent=4)
