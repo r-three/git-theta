1ae2b9c8cc24486d601cf2c3d78757c4f80d32ef
Nikhil Kandpal <nkandpa2@gmail.com>
2022-11-27 11:33:43 -0500

    fix test with empty environment variable

--- tests/checkpoints_test.py
+++ tests/checkpoints_test.py
@@ -26,6 +26,16 @@ def no_env_var():
     os.environ.update(current_env)
 
 
+@pytest.fixture
+def empty_env_var():
+    current_env = dict(os.environ)
+    os.environ["GIT_THETA_CHECKPOINT_TYPE"] = ""
+
+    yield
+    os.environ.clear()
+    os.environ.update(current_env)
+
+
 def test_get_checkpoint_handler_name_user_input(env_var):
     user_input = "user_input_handler"
     name = checkpoints.get_checkpoint_handler_name(user_input)
@@ -42,7 +52,7 @@ def test_get_checkpoint_handler_name_default1(no_env_var):
     assert name == "pytorch"
 
 
-def test_get_checkpoint_handler_name_default2(no_env_var):
+def test_get_checkpoint_handler_name_default2(empty_env_var):
     name = checkpoints.get_checkpoint_handler_name()
     assert name == "pytorch"
 
