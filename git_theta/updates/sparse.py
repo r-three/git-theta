632b1712fb1d27ef54e0ca90cf80f48db552a1e7
muqeeth <muqeeth101@gmail.com>
2023-01-11 17:14:07 -0500

    reformatting

--- git_theta/updates/sparse.py
+++ git_theta/updates/sparse.py
@@ -3,6 +3,8 @@
 import logging
 from typing import Optional, Any
 from git_theta.updates import IncrementalUpdate
+import scipy.sparse
+import numpy as np
 
 Parameter = Any
 
@@ -17,7 +19,18 @@ class SparseUpdate(IncrementalUpdate):
     async def calculate_update(
         self, parameter: Parameter, previous_parameter: Parameter
     ) -> Parameter:
-        return parameter - previous_parameter
+        diff = parameter - previous_parameter
+        update = scipy.sparse.csr_matrix(np.reshape(diff, (-1,)))
+        return {
+            "data": update.data,
+            "indices": update.indices,
+            "indptr": update.indptr,
+            "shape": parameter.shape,
+        }
 
     async def apply_update(self, update: Parameter, previous: Parameter) -> Parameter:
-        return update + previous
+        param_update = scipy.sparse.csr_matrix(
+            (update["data"], update["indices"], update["indptr"]),
+            shape=(1, np.prod(update["shape"])),
+        )
+        return np.reshape(param_update.toarray(), update["shape"]) + previous
